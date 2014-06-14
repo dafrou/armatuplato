1c3b66b1e712d3789dfe96afa5c678699f5d7789
drtobal <cristobal@cristobaldiaz.cl>
2014-06-14 17:37:51 -0400

    agregados productos de prueba

--- src/doc/base de datos/armatuplato.sql
+++ src/doc/base de datos/armatuplato.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.1
+-- version 4.0.4
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 14-06-2014 a las 21:04:40
--- Versión del servidor: 5.5.24-log
--- Versión de PHP: 5.4.3
+-- Tiempo de generación: 14-06-2014 a las 21:37:37
+-- Versión del servidor: 5.6.12-log
+-- Versión de PHP: 5.4.16
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `armatuplato`
 --
+CREATE DATABASE IF NOT EXISTS `armatuplato` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `armatuplato`;
 
 -- --------------------------------------------------------
 
@@ -49,7 +51,34 @@ CREATE TABLE IF NOT EXISTS `producto` (
   `foto` varchar(250) DEFAULT NULL,
   `descripcion` varchar(250) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=22 ;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`id`, `nombre`, `precio`, `habilitado`, `foto`, `descripcion`) VALUES
+(1, 0, 2366, 1, 'page2-img1', NULL),
+(2, 0, 3044, 1, 'page2-img2', NULL),
+(3, 0, 2886, 1, 'page2-img3', NULL),
+(4, 0, 3629, 1, 'page2-img4', NULL),
+(5, 0, 3139, 1, 'page2-img5', NULL),
+(6, 0, 2063, 1, 'page2-img6', NULL),
+(7, 0, 4787, 1, 'page2-img7', NULL),
+(8, 0, 651, 1, 'page2-img8', NULL),
+(9, 0, 1747, 1, 'page2-img9', NULL),
+(10, 0, 885, 1, 'page2-img10', NULL),
+(11, 0, 1310, 1, 'page3-img1', NULL),
+(12, 0, 4363, 1, 'page3-img2', NULL),
+(13, 0, 1285, 1, 'page3-img3', NULL),
+(14, 0, 1326, 1, 'page3-img4', NULL),
+(15, 0, 2080, 1, 'page3-img5', NULL),
+(16, 0, 1926, 1, 'page3-img6', NULL),
+(17, 0, 2856, 1, 'page3-img7', NULL),
+(18, 0, 4559, 1, 'page3-img8', NULL),
+(19, 0, 4408, 1, 'slider-img1', NULL),
+(20, 0, 2869, 1, 'slider-img2', NULL),
+(21, 0, 1415, 1, 'slider-img3', NULL);
 
 -- --------------------------------------------------------
 
