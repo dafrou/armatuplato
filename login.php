c93b602831f12c46bd6aa41fdbae73a7cad733d0
dafrou <christianmezah11@gmail.com>
2014-06-14 17:44:47 -0400

    Creecion login

---
+++ login.php
@@ -0,0 +1,84 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="src/css/reset.css" type="text/css" media="screen">
+        <link rel="stylesheet" href="src/css/style.css" type="text/css" media="screen">
+        <link rel="stylesheet" href="src/css/layout.css" type="text/css" media="screen">
+        <script src="src/js/jquery-1.7.1.min.js" type="text/javascript"></script>
+        <script src="src/js/cufon-yui.js" type="text/javascript"></script>
+        <script src="src/js/cufon-replace.js" type="text/javascript"></script>
+        <script src="src/js/Dynalight_400.font.js" type="text/javascript"></script>
+        <script src="src/js/FF-cash.js" type="text/javascript"></script>
+        <script src="src/js/tms-0.3.js" type="text/javascript"></script>
+        <script src="src/js/tms_presets.js" type="text/javascript"></script>
+        <script src="src/js/jquery.easing.1.3.js" type="text/javascript"></script>
+        <script src="src/js/jquery.equalheights.js" type="text/javascript"></script>
+        <!--[if lt IE 9]><script type="text/javascript" src="src/js/html5.js"></script><![endif]-->
+    </head>
+    <body id="page6">
+        <!--==============================header=================================-->
+        <header>
+            <div class="row-top">
+                <div class="main">
+                    <div class="wrapper">
+                        <h1><a href="index.html">ArmaTuPlato . cl<span></span></a></h1>
+                        <nav>
+                            <ul class="menu">
+                                <li><a href="index.php">Inicio</a></li>
+                                <li><a href="menu.php">Precios</a></li>
+                                <li><a href="catalogue.php">Catalogo</a></li>
+                                <li><a href="shipping.php">Costos de Envios</a></li>
+                                <li><a href="faq.php">Preguntas Frecuentes</a></li>
+                                <li><a href="contact.php">Contacto</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+            <div class="row-bot">
+                <div class="row-bot-bg">
+                    <div class="main">
+                        <h2>Tu Plato a Pedido <span>En Cualquier Momento</span></h2>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!--==============================content================================-->
+        <section id="content">
+            <div class="main">
+                <div class="wrapper">
+                    <article class="col-1">       
+                    </article>
+                    <article class="col-2">
+                        <h3 class="p1">Registrate</h3>
+                        <form id="contact-form" action="#" method="post" enctype="multipart/form-data">
+                            <fieldset>
+                                <label><span class="text-form">Mail:</span>
+                                    <input name="email" type="text" />
+                                </label>
+                                <label><span class="text-form">Contraseña</span>
+                                    <input name="pass" type="text" />
+                                </label>
+                                <div class="wrapper">
+                                    
+                                        <div class="clear"></div>
+                                        <div class="buttons"> <a class="button-2" href="#">Limpiar</a> <a class="button-2" href="#">Registrarse</a> </div>
+                                    </div>
+                                </div>
+                            </fieldset>
+                        </form>
+                    </article>
+                </div>
+            </div>
+        </section>
+        <!--==============================footer=================================-->
+        <footer>
+            <div class="main">
+                <div class="aligncenter"> </div>
+            </div>
+        </footer>
+        <script type="text/javascript">Cufon.now();</script>
+        <div align=center></div>
+
+    </body>
+</html>
