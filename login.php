7337cc30ba9bf3ea9c1aeb2f3cf09ac9a0484597
drtobal <cristobal@cristobaldiaz.cl>
2014-06-15 16:53:54 -0400

    normalizando nombres de archivos

--- login.php
+++ login.php
@@ -6,19 +6,9 @@ require('./src/srv/proceso/login.php');
 <html>
     <head>
         <meta charset="utf-8">
-        <link rel="stylesheet" href="src/css/reset.css" type="text/css" media="screen">
-        <link rel="stylesheet" href="src/css/style.css" type="text/css" media="screen">
-        <link rel="stylesheet" href="src/css/layout.css" type="text/css" media="screen">
-        <script src="src/js/jquery-1.7.1.min.js" type="text/javascript"></script>
-        <script src="src/js/cufon-yui.js" type="text/javascript"></script>
-        <script src="src/js/cufon-replace.js" type="text/javascript"></script>
-        <script src="src/js/Dynalight_400.font.js" type="text/javascript"></script>
-        <script src="src/js/FF-cash.js" type="text/javascript"></script>
-        <script src="src/js/tms-0.3.js" type="text/javascript"></script>
-        <script src="src/js/tms_presets.js" type="text/javascript"></script>
-        <script src="src/js/jquery.easing.1.3.js" type="text/javascript"></script>
-        <script src="src/js/jquery.equalheights.js" type="text/javascript"></script>
-        <!--[if lt IE 9]><script type="text/javascript" src="src/js/html5.js"></script><![endif]-->
+        <?php
+            require("./src/srv/views/header.php");
+        ?>
     </head>
     <body id="page7">
         <!--==============================header=================================-->
